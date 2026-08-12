9a50a760e4be7c3fa940ff860a199d3d483bb68f
devdubos <sergey.dub.dev@gmail.com>
2026-08-12 14:35:39 +0300

    lesson1 : first steps practice

---
+++ lesson 1/ifelifelse.py
@@ -0,0 +1,18 @@
+name = "sergey"
+age = 24
+weight = 90
+if age >= 18 and weight < 80:
+    print(f" Hi, {name}! You look so strong")
+    print("You have permission to learn Python ")
+elif age >=24 and weight > 80:
+    print(f" Listen, {name} its no over, keep goin")
+else:
+    print(f"{name} u do not ready yet, keep playing in dota")
+
+
+
+
+
+
+
+

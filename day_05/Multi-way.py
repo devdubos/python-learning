4c42035c2c09ce8c06ed0affc6b74080ac8aebf3
devdubos <sergey.dub.dev@gmail.com>
2026-08-17 16:29:32 +0300

    update, add some ex from ai, i think this repetition helps you not to forget what you've learned, but I also think you need more practice.

--- day_05/Multi-way.py
+++ day_05/Multi-way.py
@@ -12,3 +12,27 @@ else:
 customer_age = 16
 ticket_price = 10 if customer_age < 18 else 15
 print(f"Ticket price: ${ticket_price}")
+
+x = int(input())
+if x % 2 == 0:
+    if 0 <= x <= 9:
+        print("x - nubmer")
+    else:
+        print("x - clock")
+else:
+    print("not 2/ number")
+
+
+a = int(input("a: ")) 
+b = int(input("b: "))
+c = int(input("c: "))
+if a > b:
+    if a > c:
+        print("a -> max")
+    else:
+        print("c -> max")
+else:
+    if b > c:
+        print("b -> max")
+    else:
+        print("c -> max")

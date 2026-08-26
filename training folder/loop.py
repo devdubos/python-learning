85d2b0598c6cd518aaad7b29fce9834e237d2f38
devdubos <sergey.dub.dev@gmail.com>
2026-08-26 23:46:20 +0300

    refactor just some practice

--- training folder/loop.py
+++ training folder/loop.py
@@ -3,3 +3,27 @@ while count < 5:
     print("hello")
     count += 1
     
+text = "hello world"
+vowels = "aeiou"
+count = 0
+
+for char in text:
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
+
+n = 5
+
+for i in range(1, 11):
+    print(f"{n} x {i} = {n * i}")
+
+
+    num = 12345
+digit_sum = 0
+
+while num > 0:
+    digit_sum += num % 10
+    num = num // 10
+
+print("Sum of digits:", digit_sum)

29893abb57a173d1dbd376b12f7136a604bd3bf7
devdubos <sergey.dub.dev@gmail.com>
2026-08-29 00:44:40 +0300

    refactor: for

--- training folder/for.py
+++ training folder/for.py
@@ -1,6 +1,27 @@
-for i in range(3):
-    print(f"{i} hello world", end=" ")
+def task_1():
+    for i in range(3):
+        print(f"{i} hello world", end=" ")
+def task_2():
+    for i in range(11):
+        if i % 2 == 0:
+            if i < 10:
+                print(i, end="-")
+            else:
+                print(i)
 
-for i in range(11):
-    if i % 2 == 0:
-        print(i, end="-")
+def task_prices():
+    prices = [100, 250, 50, 400]
+    total = 0 
+    for i in prices:
+        total = i + total
+    print(total)
+task_prices()
+
+def task_3():
+    secret = "h-e-l-l-o"
+    for i in secret:
+        if i == "-":
+            continue
+        else:
+            print(f"{i}", end="")
+task_3()

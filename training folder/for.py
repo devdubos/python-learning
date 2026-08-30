e0e4cdaed41e32d7ec2b36d59f712eb1a0f6e458
devdubos <sergey.dub.dev@gmail.com>
2026-08-30 23:54:32 +0300

    refactor hard ex complete

--- training folder/for.py
+++ training folder/for.py
@@ -25,3 +25,22 @@ def task_3():
         else:
             print(f"{i}", end="")
 task_3()
+
+def check_bank_security():
+    logs = ["login_success", "transfer_100", "password_error", "transfer_250", "login_success", "password_error", "transfer_500"]
+    
+    errors_count = 0
+    total_money = 0
+    
+    for log in logs:
+        if log == "password_error":
+            errors_count += 1
+            
+        elif log.startswith("transfer_"):
+            money = int(log[9:])
+            total_money += money
+
+    print(f"Errors detected: {errors_count}")
+    print(f"Total money transferred: {total_money}")
+
+check_bank_security()

d67436ed8614be9d1baa7e619bb59643d8a81b75
devdubos <sergey.dub.dev@gmail.com>
2026-09-09 14:06:40 +0300

    refactor: Refresh knowlege and practice work with list, func while, for.

--- day_13/enumerate.py
+++ day_13/enumerate.py
@@ -3,13 +3,13 @@ def task_01():
 
     for index, lang in enumerate(languages):
         print(f"Index: {index}, Language: {lang}")
-task_01()
+#task_01()
 
 def task_02():
     students = ["Alex", "Mary", "John"]
     for table_num, name in enumerate(students, start=1):
         print(f"Table №{table_num}: {name}")
-task_02()
+#task_02()
 
 def task_03():
     names = ["Alice", "Bob", "Charlie", "David", "Eva"]
@@ -17,20 +17,27 @@ def task_03():
     for index, name in enumerate(names):
         if index % 2 == 0:
             print(f"Even index {index}: {name}")
-task_03()
+#task_03()
 
 def task_04():
-    users = ["user_102", "admin", "user_205", "admin", "user_301", "guest", "admin"]
+    steps_per_day = [4500, 11000, 7200, 13500, 3100]
+    for idex,steps in enumerate(steps_per_day):
+        if steps >= 10000:
+            print(f"Day{idex+1} U complete normal! Steps: {steps}") 
 
-print("=== Admin Search ===")
+def task_05():
+    prices = [1500, 450, 3000, 100, 850]
+    suma = 0
+    for i in prices:
+        suma += i
+    print(f"total suma: {suma}")
 
-for position, user_id in enumerate(users, start=1):
-    if user_id == "admin":
-        print(f"Admin found at position {position} (ID: {user_id}_{position})")
-
-print("\n=== Admin Database ===")
-
-admin_map = {pos: name for pos, name in enumerate(users, start=1) if name == "admin"}
-
-print(admin_map)
-task_04()
+def task_06():
+    prices = [1500, 450, 3000, 100, 850]
+    box = 0
+    for i, price in enumerate(prices):
+        if price > 1000:
+            box += price
+            print(f"Stuf number {i+1} worth more then 1000")
+    print(f"Price of all stufs:{box} dollars")
+task_06()

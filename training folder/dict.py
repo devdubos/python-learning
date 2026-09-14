13232e3010fe4cc2f169908830fead59998718c1
devdubos <sergey.dub.dev@gmail.com>
2026-09-14 23:55:59 +0300

    forgot about dict

--- training folder/dict.py
+++ training folder/dict.py
@@ -1,21 +1,48 @@
-student = {
-    "name": "Alex",
-    "age": 20,
-    "grades": [85, 90, 78, 92]
+# student = {
+#     "name": "Alex",
+#     "age": 20,
+#     "grades": [85, 90, 78, 92]
+# }
+# print(student["name"])
+# average_grade = sum(student["grades"]) / len(student["grades"])
+# print(average_grade)
+# student["status"] = "active"
+# print(student)
+
+# text = "programming"
+# letter_count = {}
+
+# for letter in text:
+#     if letter in letter_count:
+#         letter_count[letter] += 1
+#     else:
+#         letter_count[letter] = 1
+
+# print(letter_count)
+
+# author = {
+#     "name": "Sergey",
+#     "age": 18,
+#     "surname": "dub",
+# }
+
+# print(author.get("surname", "Duboss"))
+
+# author["age"] = 19
+# author["is_mentor"] = True
+# author.pop("name")
+# print(author)
+
+
+author = {
+    "name": "Sergey",
+    "age": 18,
+    "is_dev": True,
 }
-print(student["name"])
-average_grade = sum(student["grades"]) / len(student["grades"])
-print(average_grade)
-student["status"] = "active"
-print(student)
+print(f"keys: {author.keys()}")
 
-text = "programming"
-letter_count = {}
+for pair in author.values():
+    print(pair)
 
-for letter in text:
-    if letter in letter_count:
-        letter_count[letter] += 1
-    else:
-        letter_count[letter] = 1
 
-print(letter_count)
+
